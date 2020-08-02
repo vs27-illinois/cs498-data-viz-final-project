83b1182d0ee8fd81b058c1c37400c2064f423f6b
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-08-02 19:17:51 -0400

    commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let base_url = 'https://raw.githubusercontent.com/vs27-illinois/cs498-data-viz-final-project/master/';
 
-// Load GeoJSON data and merge with states data
+// Load GeoJSON data
 d3.json(base_url + "us-states.json")
   .then(function(json) {
     //Width and height of map
@@ -38,4 +38,34 @@ d3.json(base_url + "us-states.json")
 		.style("stroke", "#fff")
 		.style("stroke-width", "1")
 		.style("fill", "rgb(213,222,217)");
+  }).catch(err => console.log(err));
+
+d3.csv(base_url + "us-city-lat-long.csv")
+  .then(function(city) {
+    d3.csv(base_url + "police-locals.csv")
+      .then(function(data) {
+        data.forEach(function(d) {
+            city.forEach(function(c) {
+                if (c['city'] == d['city']) {
+                  d['lat'] = c['lat'];
+                  d['long'] = c['long'];
+            });
+        });
+        svg.selectAll("circle")
+             .data(data)
+             .enter()
+             .append("circle")
+             .attr("cx", function(d) {
+                return projection([d['long'], d['lat']])[0];
+             })
+             .attr("cy", function(d) {
+                return projection([d['long'], d['lat']]])[1];
+             })
+             .attr("r", function(d) {
+                return d['police_force_size'] / 100;
+             })
+             .style("fill", "rgb(217,91,67)")
+             .style("opacity", 0.85)
+        }
+      }).catch(err => console.log(err));
   }).catch(err => console.log(err));

c092709f8ac288283532f943c5de42516e7ddffa
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-08-03 16:51:40 -0400

    commit

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ let map_width = 1200;
 let map_height = 600;
 
 // Stacked Bar Chart Width and Height
-let sb_width = 600;
-let sb_height = 1200;
+let sb_width = 1000;
+let sb_height = 1500;
 
 // Width, Height and Margin of the Bar chart
-let margin = {top: 10, right: 30, bottom: 20, left: 50};
+let margin = {top: 10, right: 30, bottom: 20, left: 220};
 let bar_width = sb_width - margin.left - margin.right;
 let bar_height = sb_height - margin.top - margin.bottom;
 

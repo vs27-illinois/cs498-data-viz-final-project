1c953c61aeedde3916a3e467bba8016092125b17
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-08-03 00:01:55 -0400

    commit

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let base_url = 'https://raw.githubusercontent.com/vs27-illinois/cs498-data-viz-final-project/master/';
 
-//Width and height of map
+// SVG Width and Height
 let width = 1200;
 let height = 600;
+
+// Width, Height and Margin of the Bar chart
+let margin = {top: 10, right: 30, bottom: 20, left: 50};
+let bar_width = width - margin.left - margin.right;
+let bar_height = height - margin.top - margin.bottom;
 
 let div = d3.select("body")
             .append("div")
@@ -11,19 +16,16 @@ let div = d3.select("body")
 
 d3.csv(base_url + "police-locals.csv")
   .then(function(data) {
-    // Load GeoJSON data
     d3.json(base_url + "us-states.json")
       .then(function(json) {
-        // D3 Projection
         let projection = d3.geoAlbersUsa()
                        .translate([width/2, height/2])
                        .scale(1200);
 
-        // Define path generator
         let path = d3.geoPath()
                      .projection(projection);
 
-        //Create SVG element and append map to the SVG
+        // Create SVG element and append map to the SVG
         let svg = d3.select("#map")
     			.append("svg")
     			.attr("width", width)
@@ -31,7 +33,6 @@ d3.csv(base_url + "police-locals.csv")
     			.attr("viewBox", "0 0 " + width + " " + height)
     			.attr("preserveAspectRatio", "xMidYMid meet");
 
-        // Bind the data to the SVG and create one path per GeoJSON feature
         svg.selectAll("path")
             .data(json.features)
             .enter()
@@ -72,7 +73,7 @@ d3.csv(base_url + "police-locals.csv")
                 .style("stroke", "red")
                 .style("stroke-width", "1")
                 .on("mouseover", function(d) {
-                    let text = 'City: ' + d['city'] + '<br/>Police Force Size: ' + f(d['police_force_size']);
+                    let text = 'City: ' + d['city'] + '<br/># of Officers: ' + f(d['police_force_size']);
                     div.transition()
                        .duration(200)
                        .style("opacity", .9);
@@ -99,6 +100,13 @@ d3.csv(base_url + "police-locals.csv")
           }).catch(err => console.log(err));
       }).catch(err => console.log(err));
 
+      let svg = d3.select("stack-bar")
+                  .append("svg")
+                  .attr("width", width)
+                  .attr("height", height)
+                  .append("g")
+                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
       let dataset = d3.layout.stack()(["Local", "Non-Local"].map(function(type) {
         return data.map(function(d) {
           let value = d['police_force_size'];
@@ -108,4 +116,50 @@ d3.csv(base_url + "police-locals.csv")
           return {x: parse(d['city']), y: value};
         });
       }));
+
+      let x = d3.scale.ordinal()
+                .domain(dataset[0].map(function(d) { return d.x; }))
+                .rangeRoundBands([10, bar_width-10], 0.02);
+
+      let y = d3.scale.linear()
+                .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+                .range([bar_height, 0]);
+
+      let colors = ["b33040", "#d25c4d"];
+
+      let yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left")
+                    .ticks(5)
+                    .tickSize(-bar_width, 0, 0)
+                    .tickFormat( function(d) { return d } );
+
+      let xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom")
+                    .tickFormat( function(d) { return d } );
+
+      svg.append("g")
+         .attr("class", "y axis")
+         .call(yAxis);
+
+      svg.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + bar_height + ")")
+         .call(xAxis);
+
+      let groups = svg.selectAll("g.cost")
+                      .data(dataset)
+                      .enter().append("g")
+                      .attr("class", "cost")
+                      .style("fill", function(d, i) { return colors[i]; });
+
+      let rect = groups.selectAll("rect")
+                       .data(function(d) { return d; })
+                       .enter()
+                       .append("rect")
+                       .attr("x", function(d) { return x(d.x); })
+                       .attr("y", function(d) { return y(d.y0 + d.y); })
+                       .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+                       .attr("width", x.rangeBand());
   }).catch(err => console.log(err));

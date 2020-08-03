48163f9cea914ddf2f3cb8c3c913139647695b63
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-08-03 16:54:50 -0400

    commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let sb_width = 1000;
 let sb_height = 2000;
 
 // Width, Height and Margin of the Bar chart
-let margin = {top: 10, right: 30, bottom: 20, left: 150};
+let margin = {top: 10, right: 50, bottom: 20, left: 150};
 let bar_width = sb_width - margin.left - margin.right;
 let bar_height = sb_height - margin.top - margin.bottom;
 

f39b5ad187aa9a9cc445dbe8e92ce927da9d100f
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-08-04 17:36:20 -0400

    commit

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ let margin = {top: 30, right: 30, bottom: 30, left: 150};
 
 // Bar Chart Width, Height and Margin
 let b_width = 500;
-let b_height = 300;
-let b_margin = {top: 60, right: 30, bottom: 30, left: 50};
+let b_height = 400;
+let b_margin = {top: 100, right: 30, bottom: 30, left: 50};
 
 let f = d3.format(",");
 

136765d02b061bce67c1d36f8485f00ab7e54ae2
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-08-04 00:33:56 -0400

    commit

--- main.js
+++ main.js
@@ -305,7 +305,7 @@ function create_slide3(data) {
               .padding(0.1);
 
   let y = d3.scaleLinear()
-              .domain([0, 100]).nice()
+              .domain([0, 1]).nice()
               .range([b_height - b_margin.bottom, b_margin.top])
 
   svg.append("g")
